06aaf6639505f1d3bd1acc00e3fa5874ea57db3d
villerbond <91826226+villerbond@users.noreply.github.com>
2021-12-15 00:12:03 +0300

    Update snail.cpp

--- 08.11/snail.cpp
+++ 08.11/snail.cpp
@@ -4,120 +4,168 @@ int** createMatrix(int rows, int columns);
 void deleteMatrix(int** M, int rows);
 void printMatrix(int **M, int rows, int columns, const char pre[]);
 int** inputMatrix(int &rows, int &columns);
-void snailMatrix(int **M, int size);
+int** snailMatrix(int rows, int columns);
 
 int main() {
-  int n;
+    int n,m;
   
-  do {
-      std::cout << "Input size of matrix: ";
-      std::cin >> n;
-  } while (n < 1);
+    do {
+        std::cout << "Input row's number: ";
+        std::cin >> n;
+        std::cout << "Input column's number: ";
+        std::cin >> m;
+    } while (n < 1 or m < 1);
     
-  int **I = inputMatrix(n,n);
-  printMatrix(I,n,n,"I = \n");
-  std::cout << std::endl;
-  snailMatrix(I,n);
-  
-  deleteMatrix(I,n);
-  return 0;
+    int** I = snailMatrix(n,m);
+    printMatrix(I,n,m,"I = \n");
+    deleteMatrix(I,n);
+    return 0;
 }
 
 int** createMatrix(int rows, int columns) {
   
-  int** M = new int*[rows];
-
-  for (int i = 0; i < rows; ++i) {
-    M[i] = new int[columns];
-  }
-  
-  return M;
+    int** M = new int*[rows];
+    for (int i = 0; i < rows; ++i) {
+        M[i] = new int[columns];
+    }
+    return M;
 }
 
 void deleteMatrix(int** M, int rows) {
-  for (int i = 0; i < rows; ++i) {
-    delete[] M[i];
-  }
-  delete[] M;
+    for (int i = 0; i < rows; ++i) {
+        delete[] M[i];
+    }
+    delete[] M;
 }
 
 void printMatrix(int **M, int rows, int columns, const char pre[]) {
-  std::cout << pre;
-  for (int i = 0; i < rows; ++i) {
-    for (int j = 0; j < columns; ++j) {
-      std::cout << M[i][j] << "\t";
-    }
+    std::cout << pre;
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < columns; ++j) {
+            std::cout << M[i][j] << "\t";
+        }
     std::cout << std::endl;
-  }
+    }
 }
 
-int** inputMatrix(int &rows, int &columns) {
-  int** M = createMatrix(rows,columns);
-  for (int i = 0; i < rows; ++i) {
-    for (int j = 0; j < columns; ++j) {
-      std::cout << "matrix(" << i+1 << "," << j+1 << ") = ";
-      std::cin >> M[i][j];
-    }
-  }
+int** snailMatrix(int rows, int columns) {
 
-  return M;
-}
+    int** M = createMatrix(rows,columns);
+    int x = 0; // current cell --- like rows
+    int y = 0; // current cell | like columns
+    short dir = 1; // direction;
 
-void snailMatrix(int **M, int size) {
-
-  int x = 0; // current cell --- like rows
-  int y = 0; // current cell | like columns
+    // 0 - right
+    // 1 - down
+    // 2 - left
+    // 3 - up
   
-  short dir = 1; // direction;
-
-  // 0 - right
-  // 1 - down
-  // 2 - left
-  // 3 - up
-
- //---First_print---right
-
-  for (int i = 0; i < size; ++i) {
-    std::cout << M[x][y] << " ";
-    y++;
-  }
-  y--;
+    //---Next_prints---for square matrix and for rectangle matrix
   
-//---Next_prints---
-  
-  int steps = size - 1; // how much to print at time 
-  short int flag = 1; // guarantees every step two times
-  
-  while (steps != 0) {
-    switch (dir) {
+    // square matrix
 
-      case 0: // right
-        for (int i = 0; i < steps; ++i) {
-          y++;
-          std::cout << M[x][y] << " ";
-        }; break;
+    if (rows == columns) {
+        //---First_print---right
+        int temp = 1;
+        int size = rows;
+        for (int i = 0; i < size; ++i) {
+            M[x][y] = temp++;
+            y++;
+        }
+        y--;
+  
+        int steps = size - 1; // how much to print at time 
+        short int flag = 1; // guarantees every step two times
+  
+        while (steps != 0) {
+            switch (dir) {
+
+                case 0: // right
+                    for (int i = 0; i < steps; ++i) {
+                        y++;
+                        M[x][y] = temp++;
+                    }; break;
       
-      case 1: // down
-        for(int i = 0; i < steps; ++i) {
-          x++;
-          std::cout << M[x][y] << " ";
-        }; break;
+                case 1: // down
+                    for(int i = 0; i < steps; ++i) {
+                        x++;
+                        M[x][y] = temp++;
+                    }; break;
       
-      case 2: // left
-        for (int i = 0; i < steps; ++i) {
-          y--;
-          std::cout << M[x][y] << " ";
-        }; break;
+                case 2: // left
+                    for (int i = 0; i < steps; ++i) {
+                        y--;
+                        M[x][y] = temp++;
+                    }; break;
       
-      case 3: // up
-        for (int i = 0; i < steps; ++i) {
-          x--;
-          std::cout << M[x][y] << " ";
-        }; break;
-    }
+                case 3: // up
+                    for (int i = 0; i < steps; ++i) {
+                        x--;
+                        M[x][y] = temp++;
+                    }; break;
+            }
     
-    flag = (flag + 1) % 2;
-    steps = steps - 1 * flag; // "steps" will decrease every two iterates of cycle
-    dir = (dir + 1) % 4; // "dir" will change from 0 to 3 every iterate of cycle, it guarantees change of print direction
-  } 
+            flag = (flag + 1) % 2;
+            steps = steps - 1 * flag; // "steps" will decrease every two iterates of cycle
+            dir = (dir + 1) % 4; // "dir" will change from 0 to 3 every iterate of cycle, it guarantees change of print direction
+        } 
+    } 
+  
+  //rectangle matrix
+  
+    else {
+      
+        int temp = 1;
+      
+        // ---First_print---right
+      
+        for (int i = 0; i < columns; ++i) {
+            M[x][y] = temp++;
+            y++;
+        }
+        y--;
+      
+        int stepsx = rows -  1;
+        int stepsy = columns - 1;
+      
+        while (stepsx != -1 and stepsy != -1) {
+            switch (dir) {
+            
+                case 0: // right
+                    for (int i = 0; i < stepsy; ++i) {
+                        y++;
+                            M[x][y] = temp++;
+                    }; 
+                    stepsy--;
+                    break;
+      
+                case 1: // down
+                    for(int i = 0; i < stepsx; ++i) {
+                        x++;
+                            M[x][y] = temp++;
+                    }; 
+                    stepsx--;
+                    break;
+      
+                case 2: // left
+                    for (int i = 0; i < stepsy; ++i) {
+                        y--;
+                            M[x][y] = temp++;
+                    }; 
+                    stepsy--;
+                    break;
+      
+                case 3: // up
+                    for (int i = 0; i < stepsx; ++i) {
+                        x--;
+                        M[x][y] = temp++;
+                    }; 
+                    stepsx--;
+                    break;
+            
+            }
+            dir = (dir + 1) % 4;
+        }
+    }
+    return M;
 }
