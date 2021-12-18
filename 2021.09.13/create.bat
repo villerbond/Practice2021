0e4647716d8b1524a354658953885b46c8a2629c
villerbond <91826226+villerbond@users.noreply.github.com>
2021-12-18 19:59:08 +0300

    Rename 13.09/create.bat to 2021.09.13/create.bat

---
+++ 2021.09.13/create.bat
@@ -0,0 +1,28 @@
+@ECHO OFF
+
+md semest1
+cd semest1
+
+md algem
+cd algem
+echo Âåäÿêîâà Àíàñòàñèÿ Îëåãîâíà > readme.txt
+echo Íà÷àëî çàíÿòèé: 20.09 >> readme.txt
+cd..
+
+md matanaliz
+cd matanaliz
+echo Åñòàôüåâà Âèêòîðèÿ Âèêòîðîâíà > readme.txt
+echo Íà÷àëî çàíÿòèé: 20.09 >> readme.txt
+cd..
+
+md proga
+cd proga
+echo Ïîãîæåâ Ñåðãåé Âëàäèìèðîâè÷ > readme.txt
+echo Íà÷àëî çàíÿòèé: 20.09 >> readme.txt
+cd..
+
+md diskretka
+cd diskretka
+echo diskretka > readme.txt
+echo Íà÷àëî çàíÿòèé: 24.09 >> readme.txt
+cd..
