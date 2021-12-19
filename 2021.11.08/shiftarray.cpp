27045e3dc8b658a3caadd54f417a6cba5e566dbb
villerbond <91826226+villerbond@users.noreply.github.com>
2021-12-20 00:14:13 +0300

    Update shiftarray.cpp

--- 2021.11.08/shiftarray.cpp
+++ 2021.11.08/shiftarray.cpp
@@ -6,7 +6,12 @@ void shiftArray(int A[], int n, int k);
 int main() {
 
     int A[10]= {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    shiftArray(A, 10, 3);
+    
+    int sh;
+    std::cout << "Input positive shift: ";
+    std::cin >> sh;
+    
+    shiftArray(A, 10, sh);
     
     for (int i = 0; i < 10; ++i) {
         std::cout << A[i] << " ";
@@ -25,10 +30,15 @@ void reverseArray(int* A, int begin, int end) {
 
 void shiftArray(int A[], int n, int k) {
     
+    k = k % n;
+    
+    if (k < 0) {
+        k = -k;
+        k = n-k;
+    }
+    
     reverseArray(A, 1, k);
-    
     reverseArray(A, k+1, n);
-    
-    reverseArray(A,1,n);
+    reverseArray(A, 1, n);
     
 }
