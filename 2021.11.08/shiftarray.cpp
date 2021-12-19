a63ee54f39adb1f0c57080285bd5c9d5c12c3148
villerbond <91826226+villerbond@users.noreply.github.com>
2021-12-20 00:14:42 +0300

    Update shiftarray.cpp

--- 2021.11.08/shiftarray.cpp
+++ 2021.11.08/shiftarray.cpp
@@ -8,7 +8,7 @@ int main() {
     int A[10]= {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
     
     int sh;
-    std::cout << "Input positive shift: ";
+    std::cout << "Input shift: ";
     std::cin >> sh;
     
     shiftArray(A, 10, sh);
